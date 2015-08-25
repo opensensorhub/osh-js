e1a19ab3bc85da9f11ba52470e0184f94ffd8619
Mathieu dhainaut <mathieu.dhainaut@gmail.com>
2015-08-25 21:27:21 +0000

    update urls;change default widgets place

--- GpsStreamViewer/viewers/Temperature/js/gauge.js
+++ GpsStreamViewer/viewers/Temperature/js/gauge.js
@@ -8,7 +8,7 @@ function Gauge(placeholderName, configuration)
 	{
 		this.config = configuration;
 		
-		this.config.size = this.config.size * 0.9;
+		this.config.size = this.config.size;
 		
 		this.config.raduis = this.config.size * 0.97 / 2;
 		this.config.cx = this.config.size / 2;
