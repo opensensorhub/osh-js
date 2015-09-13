03913a5e3347d5fe4e878fa1fe4f30dc7ba2b79e
Mathieu dhainaut <mathieu.dhainaut@gmail.com>
2015-09-13 00:31:42 +0000

    prettify code

--- RangeFinder/common/js/sync.js
+++ RangeFinder/common/js/sync.js
@@ -158,3 +158,10 @@ Buffer.prototype.switchMode = function(mode){
 Buffer.prototype.getMode = function(){
 	return this.currentMode;
 }
+
+function getUUID(){
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+}
