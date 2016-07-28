0e9552fd72fd5b38659eca43b9619dac11fcde37
alexrobin <alex.robin@sensiasoftware.com>
2016-07-28 10:23:10 -0500

    New Euler orientation receiver

---
+++ Toolkit/src/osh/datareceiver/osh-DataReceiver-EulerOrientation.js
@@ -0,0 +1,23 @@
+OSH.DataReceiver.EulerOrientation = Class.create(OSH.DataReceiver.DataSource,{
+
+  parseTimeStamp: function($super,data){
+    var rec = String.fromCharCode.apply(null, new Uint8Array(data));
+    var tokens = rec.trim().split(",");
+    var t =  new Date(tokens[0]).getTime();
+    return t - this.androidShift;
+  },
+  
+  parseData: function($super,data){
+    var rec = String.fromCharCode.apply(null, new Uint8Array(data));
+    var tokens = rec.trim().split(",");
+    var yaw = parseFloat(tokens[1]);    
+    var pitch = parseFloat(tokens[2]);
+    var roll = parseFloat(tokens[3]);
+    
+    return {
+      pitch : pitch,
+      roll : roll,
+      heading: yaw
+    };
+  } 
+});
