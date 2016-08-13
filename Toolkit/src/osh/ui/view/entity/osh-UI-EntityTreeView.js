fd6ad8d325d66149c88fe1b21411a5fa59800afa
alexrobin <alex.robin@sensiasoftware.com>
2016-08-13 15:26:46 -0500

    Append items to same folder if they share a common path. Closes #24

--- Toolkit/src/osh/ui/view/entity/osh-UI-EntityTreeView.js
+++ Toolkit/src/osh/ui/view/entity/osh-UI-EntityTreeView.js
@@ -20,23 +20,42 @@ OSH.UI.EntityTreeView = Class.create(OSH.UI.View,{
             if(path.endsWith("/")) {
                 path = path.substring(0,path.length-1);
             }
+            
+            // create intermediary folders or append to them as needed 
             var folder = path.split("/");
             var nbNodes = folder.length;
-            var currentNode = null;
+            var currentNode = this.tree;
             var pos = 0;
             while(nbNodes > 0) {
-                if(currentNode == null) {
-                    currentNode = this.tree.createNode(folder[pos],false,'',null,null);
-                } else {
-                    currentNode = currentNode.createChildNode(folder[pos],false,'',null,null);
+            	var existingChildNode = null;
+            	
+            	// scan child nodes to see if folder already exists
+            	for (n=0; n<currentNode.childNodes.length; n++) {
+            		var node = currentNode.childNodes[n];
+            		if (node.text === folder[pos]) {
+            			existingChildNode = node;
+            		    break;
+            		}
                 }
+            	
+            	// if folder already exists, just use it as parent in next iteration
+            	// otherwise create a new node to use as new parent
+            	if (existingChildNode == null) {
+            		if (currentNode === this.tree)
+            			currentNode = this.tree.createNode(folder[pos],false,'',null,null);
+            		else
+            			currentNode = currentNode.createChildNode(folder[pos],false,'',null,null);    
+                } else {
+                	currentNode = existingChildNode;
+                }
+            	
                 pos++;
                 nbNodes--;
             }
-            if(currentNode != null) {
-                var leaf = currentNode.createChildNode(entity.name, false, treeIcon, null, contextMenuId);
+            if(currentNode === this.tree) {
+            	this.tree.createNode(entity.name,false,treeIcon,null, contextMenuId);
             } else {
-                this.tree.createNode(entity.name,false,treeIcon,null, contextMenuId);
+            	currentNode.createChildNode(entity.name, false, treeIcon, null, contextMenuId);
             }
         }
 
