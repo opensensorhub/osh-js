e5a706d26c20fe57d92845c75272352f2b3f05b6
Dhainaut mathieu <mathieu.dhainaut@gmail.com>
2016-09-06 14:35:22 +0200

    remove usunsed version

--- Toolkit/src/osh/osh-template.js
+++ Toolkit/src/osh/osh-template.js
@@ -11,88 +11,52 @@ window.OSH = OSH;
  * @namespace {object} OSH.Video
  * @memberof OSH
  */
-OSH.Video = {
-	version: 'dev'
-};
-
-window.OSH.Video = OSH.Video;
+window.OSH.Video = {};
 
 /**
  * @namespace {object} OSH.UI
  * @memberof OSH
  */
-OSH.UI = {
-	version: 'dev'
-};
-
-window.OSH.UI = OSH.UI;
+window.OSH.UI = {};
 
 /**
  * @namespace {object} OSH.UI.View
  * @memberof OSH.UI
  */
-OSH.UI.View = {
-	version: 'dev'
-};
-
-window.OSH.UI.View = OSH.UI.View;
+window.OSH.UI.View = {};
 
 /**
  * @namespace {object} OSH.UI.Styler
  * @memberof OSH.UI
  */
-OSH.UI.Styler = {
-	version: 'dev'
-};
-
-window.OSH.Styler = OSH.Styler;
+window.OSH.Styler = {};
 
 /**
  * @namespace {object} OSH.UI.ContextMenu
  * @memberof OSH.UI
  */
-OSH.UI.ContextMenu = {
-	version: 'dev'
-};
-
-window.OSH.ContextMenu = OSH.ContextMenu;
+window.OSH.ContextMenu = {};
 
 /**
  * @namespace {object} OSH.DataReceiver
  * @memberof OSH
  */
-OSH.DataReceiver = {
-	version: 'dev'
-};
-
-window.OSH.DataReceiver = OSH.DataReceiver;
+window.OSH.DataReceiver = {};
 
 /**
  * @namespace {object} OSH.DataConnector
  * @memberof OSH
  */
-OSH.DataConnector = {
-	version: 'dev'
-};
-
-window.OSH.DataConnector = OSH.DataConnector;
+window.OSH.DataConnector = {};
 
 /**
  * @namespace {object} OSH.Utils
  * @memberof OSH
  */
-OSH.Utils = {
-	version: 'dev'
-};
-
-window.OSH.Utils = OSH.Utils;
+window.OSH.Utils = {};
 
 /**
  * @namespace {object} OSH.DataSender
  * @memberof OSH
  */
-OSH.DataSender = {
-	version: 'dev'
-};
-
-window.OSH.DataSender = OSH.DataSender;
+window.OSH.DataSender = {};
