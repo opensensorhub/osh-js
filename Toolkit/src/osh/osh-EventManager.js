e384c0032e40dca7404dae74accb461092df2278
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2017-02-01 21:22:15 +0100

    Fix bower after moving code into Toolkit dir + add Leaflet.Fullscreen support

--- Toolkit/src/osh/osh-EventManager.js
+++ Toolkit/src/osh/osh-EventManager.js
@@ -1,59 +1,10 @@
-var observedEvent = {};
-
 /**
  *
  * @constructor
  */
 OSH.EventManager = function() {};
 
-/**
- *
- * The following are internal methods used by the Event Manager itself
- * primarily to deal with cross platform issues when firing an event
- */
-OSH.EventManager.getFireTarget = function(element) {
-    if (element !== document)
-        return element;
-    if (document.createEvent && !element.dispatchEvent)
-      return document.documentElement;
-    return element;
-};
-
-OSH.EventManager.fireEvent_STD = function(element, eventName, memo, bubble) {
-    var event = document.createEvent('HTMLEvents');
-    event.initEvent(eventName, bubble, true);
-    event.memo = memo;
-    element.dispatchEvent(event);
-    return event;
-};
-
-
-OSH.EventManager.fireEvent_IE = function(element, eventName, memo, bubble) {
-    var event = document.createEventObject();
-    event.eventType = eventName;
-    event.cancelBubble = !bubble;
-    event.memo = memo;
-    element.fireEvent(event.eventType, event);
-    return event;
-};
-
-OSH.EventManager.fireEvent = function(element, eventName, properties, bubble) {
-    if(typeof element == 'string')
-        element = OSH.EventManager.getFireTarget(document.getElementById(element));
-    else
-        element = OSH.EventManager.getFireTarget(element);
-
-    if (typeof(bubble) == 'undefined')
-        bubble = true;
-    properties = properties || {};
-
-    if(document.createEvent)
-        return OSH.EventManager.fireEvent_STD(element, eventName, properties, bubble);
-    else
-        return OSH.EventManager.fireEvent_IE(element, eventName, properties, bubble);
-};
-
-
+var mapEvent = new OSH.MapEvent();
 /**
  *
  * @param eventName
@@ -62,7 +13,7 @@ OSH.EventManager.fireEvent = function(element, eventName, properties, bubble) {
  * @memberof OSH.EventManager
  */
 OSH.EventManager.fire = function(eventName, properties) {
-    OSH.EventManager.fireEvent(document, "osh:"+eventName, properties);
+    mapEvent.fire(eventName,properties);
 };
 
 /**
@@ -73,30 +24,8 @@ OSH.EventManager.fire = function(eventName, properties) {
  * @instance
  * @memberof OSH.EventManager
  */
-OSH.EventManager.observe = function(eventName, fnCallback, id) {
-    var handleEvent = function (event) {
-        if(typeof fnCallback != "undefined") {
-            fnCallback(event.memo);
-        }
-    };
-    if(typeof  id != "undefined") {
-        observedEvent[id] = handleEvent;
-    }
-    OSH.DomEvent.on(document, "osh:"+eventName, handleEvent);
-};
-
-/**
- *
- * @param eventName
- * @param id
- * @instance
- * @memberof OSH.EventManager
- */
-OSH.EventManager.stopObserving = function(eventName, id) {
-    if(typeof id != "undefined") {
-        OSH.DomEvent.off(document, eventName, observedEvent[id]);
-        delete observedEvent[id];
-    }
+OSH.EventManager.observe = function(eventName, fnCallback) {
+    mapEvent.observe(eventName,fnCallback);
 };
 
 /**
@@ -109,11 +38,8 @@ OSH.EventManager.stopObserving = function(eventName, id) {
  */
 OSH.EventManager.observeDiv = function(divId, eventName, fnCallback) {
     elem = document.getElementById(divId);
-    OSH.DomEvent.on(elem, eventName, function(event) {
-        if(typeof fnCallback != "undefined") {
-            fnCallback(event);
-        }
-    });
+    // use native dom event listener
+    elem.addEventListener(eventName,fnCallback);
 };
 
 /**
