b5a2fde2b1f6024a2000bb8293a7b90be9b807d5
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2017-03-04 19:06:02 +0100

    #37 Add Licence/Headers on JS files

--- Toolkit/src/osh/datareceiver/osh-DataReceiver-DataSourceNexrad.js
+++ Toolkit/src/osh/datareceiver/osh-DataReceiver-DataSourceNexrad.js
@@ -1,3 +1,19 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are subject to the Mozilla Public License, v. 2.0.
+ If a copy of the MPL was not distributed with this file, You can obtain one
+ at http://mozilla.org/MPL/2.0/.
+
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+
+ Copyright (C) 2012-2016 Sensia Software LLC. All Rights Reserved.
+
+ Author: Alex Robin <alex.robin@sensiasoftware.com>
+
+ ******************************* END LICENSE BLOCK ***************************/
+
 /**
  * @classdesc This datasource provides parsing to Nexrad.
  * @class OSH.DataReceiver.Nexrad
