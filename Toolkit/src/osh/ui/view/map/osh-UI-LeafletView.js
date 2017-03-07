d560811290f3dfa1f4e4065b218bda9c424b5277
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2017-03-07 21:18:09 +0100

    [LeafletView] Replace default mapbox layer by OSM tile layer

--- Toolkit/src/osh/ui/view/map/osh-UI-LeafletView.js
+++ Toolkit/src/osh/ui/view/map/osh-UI-LeafletView.js
@@ -168,11 +168,9 @@ OSH.UI.LeafletView = OSH.UI.View.extend({
             maxZoom = options.maxZoom;
         }
         // copyrights
-        var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
-
+        var mbAttr = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+            mbUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        
         var esriLink = '<a href="http://www.esri.com/">Esri</a>';
         var esriWholink = 'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
 
