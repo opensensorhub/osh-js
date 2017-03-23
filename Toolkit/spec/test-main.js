5689ec5799b6d88350fc24c8e3adb5d380a36388
Richard Becker <beckerr@prominentedge.com>
2017-03-23 12:41:20 -0400

    Fixed file gathering for running tests. Only take files from 'spec' folder with name matching *.spec.js

--- Toolkit/spec/test-main.js
+++ Toolkit/spec/test-main.js
@@ -8,9 +8,10 @@ Object.keys(window.__karma__.files).forEach(function(file) {
     // Normalize paths to RequireJS module names.
     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
     // then do not normalize the paths
+
     var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
-    console.log(normalizedTestModule);
-    allTestFiles.push(normalizedTestModule);
+    if(normalizedTestModule.indexOf('spec/') != -1)
+      allTestFiles.push(normalizedTestModule);
   }
 });
 
