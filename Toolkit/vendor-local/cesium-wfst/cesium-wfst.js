50a16c305f3b3ba04468883205f11bf366f94de5
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2017-08-15 17:54:16 +0200

    Remove log

--- Toolkit/vendor-local/cesium-wfst/cesium-wfst.js
+++ Toolkit/vendor-local/cesium-wfst/cesium-wfst.js
@@ -462,7 +462,6 @@
         httpConnector.open("POST", this.url, true);
         httpConnector.setRequestHeader('Content-Type', 'text/xml');
 
-        console.log(payload);
         httpConnector.send(payload);
 
         httpConnector.onreadystatechange = function() {
