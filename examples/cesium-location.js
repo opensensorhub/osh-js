4fa4b266dca5de34275233af99f1fdd4fc2b541c
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2020-03-19 15:06:05 +0100

    Replace "

--- examples/cesium-location.js
+++ examples/cesium-location.js
@@ -1,20 +1,20 @@
-import Json from "../Toolkit/src/osh/datareceiver/osh-DataReceiver-DataSourceJSON";
-import CesiumView from "../Toolkit/src/osh/ui/view/map/osh-UI-CesiumView";
-import EllipsoidTerrainProvider from "cesium/Source/Core/EllipsoidTerrainProvider";
-import PointMarker from "../Toolkit/src/osh/ui/styler/osh-UI-StylerPointMarker";
+import Json from '../Toolkit/src/osh/datareceiver/osh-DataReceiver-DataSourceJSON';
+import CesiumView from '../Toolkit/src/osh/ui/view/map/osh-UI-CesiumView';
+import EllipsoidTerrainProvider from 'cesium/Source/Core/EllipsoidTerrainProvider';
+import PointMarker from '../Toolkit/src/osh/ui/styler/osh-UI-StylerPointMarker';
 
 
 window.CESIUM_BASE_URL = './';
 
 // create data source for Android phone GPS
-var gpsDataSource = new Json("android-GPS", {
-    protocol: "ws",
-    service: "SOS",
-    endpointUrl: "sensiasoft.net:8181/sensorhub/sos",
-    offeringID: "urn:android:device:060693280a28e015-sos",
-    observedProperty: "http://sensorml.com/ont/swe/property/Location",
-    startTime: "2015-02-16T07:58:30Z",
-    endTime: "2015-02-16T08:09:00Z",
+var gpsDataSource = new Json('android-GPS', {
+    protocol: 'ws',
+    service: 'SOS',
+    endpointUrl: 'sensiasoft.net:8181/sensorhub/sos',
+    offeringID: 'urn:android:device:060693280a28e015-sos',
+    observedProperty: 'http://sensorml.com/ont/swe/property/Location',
+    startTime: '2015-02-16T07:58:30Z',
+    endTime: '2015-02-16T08:09:00Z',
     replaySpeed: 2
 });
 
@@ -37,10 +37,10 @@ var pointMarker = new PointMarker({
 });
 
 // create Cesium view
-var cesiumView = new CesiumView("cesium-container",
+var cesiumView = new CesiumView('cesium-container',
     [{
         styler: pointMarker,
-        name: "Android Phone GPS"
+        name: 'Android Phone GPS'
     }]
 );
 cesiumView.viewer.terrainProvider = new EllipsoidTerrainProvider();
