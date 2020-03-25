cc326cbe0cacc18f155f0a91e6cbdc62adc2cf35
Ian Patterson <cr31.dev@gmail.com>
2020-03-25 13:37:38 -0500

    update css menu with missing changes from ProjectNYS/ProjectSCIRA

--- Toolkit/src/osh/ui/contextmenu/osh-UI-ContextMenu-CssMenu.js
+++ Toolkit/src/osh/ui/contextmenu/osh-UI-ContextMenu-CssMenu.js
@@ -193,5 +193,121 @@ OSH.UI.ContextMenu.CssMenu = OSH.UI.ContextMenu.extend({
         var matches = regExp.exec(transform);
 
         return matches[1];
+    },
+
+    // TODO: implement into toolkit source
+    /**
+     *
+     */
+    manualShow: function (properties) {
+        this.removeElement();
+
+        // TODO: make sure this is stable
+        var body = document.body;
+        var circMenuList = Array.from(document.getElementsByClassName('circular-menu-container'));
+        console.log(circMenuList);
+        for (var el in circMenuList) {
+            // console.debug('Removing: ', circMenuList[el]);
+            body.removeChild(circMenuList[el]);
+        }
+        // *******************************************
+
+        if (properties !== null || properties !== undefined) {   // TODO: check that properties isn't an empty object
+            // console.debug('Manually Showing This Menu');
+            var closeId = OSH.Utils.randomUUID();
+            var videoId = OSH.Utils.randomUUID();
+
+            var htmlVar = "";
+            htmlVar += "<div class=\"" + this.type + "-menu\">";
+            htmlVar += "  <div class=\"" + this.type + "-menu-circle\">";
+
+            // console.debug(this.items);
+
+            // adds items
+            for (var i = 0; i < this.items.length; i++) {
+                htmlVar += this.items[i].html;
+            }
+            htmlVar += "  <\/div>";
+            htmlVar += "  <a id=\"" + closeId + "\"class=\"" + this.type + "-menu-button fa fa-times fa-2x\"><\/a>";
+            htmlVar += "<\/div>";
+
+            // var outerDiv = document.createElement('div');
+            // var innerDiv = document.createElement('div');
+            // outerDiv.className = this.type + '-menu';
+            // innerDiv.className = this.type + '-menu-circle';
+
+
+            this.rootTag = document.createElement("div");
+            this.rootTag.setAttribute("class", "" + this.type + "-menu-container");
+            // TODO: verify that position is correct
+            this.rootTag.style.top = properties.y + 'px';
+            this.rootTag.style.left = properties.x + 'px';
+            this.rootTag.innerHTML = htmlVar;
+
+            document.body.appendChild(this.rootTag);
+
+            var items = document.querySelectorAll('.' + this.type + '-menu-circle a');
+
+            for (var i = 0, l = items.length; i < l; i++) {
+                items[i].style.left = (50 - 35 * Math.cos(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
+                items[i].style.top = (50 + 35 * Math.sin(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
+            }
+
+            var closeButton = document.getElementById(closeId);
+            closeButton.onclick = this.hide.bind(this);
+
+            var offsetX = 0;
+            var offsetY = 0;
+
+            if (properties.offsetX) {
+                offsetX = properties.offsetX;
+            }
+
+            if (properties.offsetY) {
+                offsetY = properties.offsetY;
+            }
+
+            document.querySelector('.' + this.type + '-menu-circle').classList.toggle('open');
+
+            if (typeof properties.x != "undefined") {
+                this.rootTag.style.left = properties.x + offsetX + 'px';
+            }
+            if (typeof properties.y != "undefined") {
+                this.rootTag.style.top = properties.y + offsetY + 'px';
+            }
+
+            // binds actions based on items
+            this.bindEvents = {};
+            for (var i = 0; i < this.items.length; i++) {
+                var item = this.items[i];
+                // console.log(item);
+                this.bindEvents[item.id] = item.viewId;
+                // TODO: add to osh-js source if effective
+                if (item.clickOverride !== undefined) {
+                    document.getElementById(item.id).onclick = item.clickOverride.bind(this);
+                } else {
+                    document.getElementById(item.id).onclick = function (event) {
+                        OSH.EventManager.fire(OSH.EventManager.EVENT.SHOW_VIEW, {
+                            viewId: this.bindEvents[event.target.id]
+                        });
+                    }.bind(this);
+                }
+            }
+
+            // this causes preventing any closing event
+            this.rootTag.onmousedown = function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+            };
+
+            // prevent unintentional opening of the default context menu
+            this.rootTag.addEventListener('contextmenu', (function) evt.preventDefault(), {capture: true});
+
+            return true;
+        } else {
+            console.error('Argument: Properties is required');
+            return false;
+        }
+
     }
 });
