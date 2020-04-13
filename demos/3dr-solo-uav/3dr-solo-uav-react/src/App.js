ccb87bc682b6e7c0ccce8aa901996990c516cc18
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2020-04-14 00:17:27 +0200

    Init 3dr solo uav as react demo

---
+++ demos/3dr-solo-uav/3dr-solo-uav-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BaseMap from "./BaseMap";
+
+function App() {
+  return (
+    <div className="App">
+      <BaseMap/>
+    </div>
+  );
+}
+
+export default App;
