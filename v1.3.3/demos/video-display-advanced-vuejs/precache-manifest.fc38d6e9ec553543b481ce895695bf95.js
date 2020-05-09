bc9e9d88534c848df343a1d8f210a0087ee7e7cf
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2020-05-09 02:30:12 +0200

    Add demos & documentation

---
+++ v1.3.3/demos/video-display-advanced-vuejs/precache-manifest.fc38d6e9ec553543b481ce895695bf95.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3046f403e69b2f65239",
+    "url": "/css/app.da512b23.css"
+  },
+  {
+    "revision": "2a1588a33099fc82630a",
+    "url": "/css/chunk-vendors.33242dd0.css"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/fonts/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/fonts/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/fonts/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/fonts/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "c0eca198d73f36427ff87b5b23f5449c",
+    "url": "/images/car-location.png"
+  },
+  {
+    "revision": "6027368fb8e86a9e4d1f00bf538bfa36",
+    "url": "/images/car-topview.png"
+  },
+  {
+    "revision": "307d9e409042a40df12a22383d716341",
+    "url": "/images/logo.png"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/img/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "c8902aeaf57b62763f49ae4ecdeccc63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3046f403e69b2f65239",
+    "url": "/js/app.a4835277.js"
+  },
+  {
+    "revision": "2a1588a33099fc82630a",
+    "url": "/js/chunk-vendors.be7f55b8.js"
+  },
+  {
+    "revision": "ccd6173c571b8905efaf6a3710bd655b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
