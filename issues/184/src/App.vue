a72699334966cdfaad9a8b11e01b19d5b263a500
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2020-06-20 11:48:23 +0200

    Add example

---
+++ issues/184/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Video
+            :draggable="true"
+            :modal="true"
+            :dataSource="dataSource"
+            :options="{top: '50', left: '50'}"
+    />
+  </div>
+</template>
+<script>
+    // @ is an alias to /src
+    import Video from 'osh-vue/components/Video.vue';
+    import VideoOsh from "osh/datareceiver/Video.js";
+
+    export default {
+        components: {
+            Video
+        },
+        data: function () {
+            return {
+                dataSource: null
+            }
+        },
+        beforeMount() {
+            // setup video
+            // create data source for UAV camera
+            this.dataSource = new VideoOsh("drone-Video", {
+                protocol: 'ws',
+                service: 'SOS',
+                endpointUrl: 'sensiasoft.net:8181/sensorhub/sos',
+                offeringID: 'urn:mysos:solo:video2',
+                observedProperty: 'http://sensorml.com/ont/swe/property/VideoFrame',
+                startTime: '2015-12-19T21:04:29.231Z',
+                endTime: '2015-12-19T21:09:19.675Z',
+                replaySpeed: 1
+            });
+        }
+    };
+</script>
+<style>
+  body {
+    overflow-x: hidden;
+    margin: 0;
+  }
+</style>
