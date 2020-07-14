2bc9be9f1aa024f7c8f9279a272c43cb8335d431
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2020-07-15 00:13:19 +0200

    Update 3dr vuejs demo to use Data synchronizer between datasources

--- demos/3dr-solo-uav/3dr-solo-uav-vuejs/src/components/Video.vue
+++ demos/3dr-solo-uav/3dr-solo-uav-vuejs/src/components/Video.vue
@@ -10,7 +10,6 @@
 
 <script>
 import DialogDrag from 'vue-dialog-drag';
-import VideoH264 from "osh/datareceiver/VideoH264.js";
 import FFMPEGView from "osh/ui/view/video/FFMPEGView.js";
 
 export default {
@@ -18,32 +17,16 @@ export default {
   components: {
     DialogDrag
   },
+  props: ['videoDataSource'],
   mounted() {
-
-      // setup video
-      // create data source for UAV camera
-      let videoDataSource = new VideoH264("drone-Video", {
-          protocol: 'ws',
-          service: 'SOS',
-          endpointUrl: 'sensiasoft.net:8181/sensorhub/sos',
-          offeringID: 'urn:mysos:solo:video2',
-          observedProperty: 'http://sensorml.com/ont/swe/property/VideoFrame',
-          startTime: '2015-12-19T21:04:29.231Z',
-          endTime: '2015-12-19T21:09:19.675Z',
-          replaySpeed: 1
-      });
-
       // show it in video view using FFMPEG JS decoder
       let videoView = new FFMPEGView("video-container", {
-          dataSourceId: videoDataSource.id,
+          dataSourceId: this.videoDataSource.id,
           css: "video-h264",
           name: "UAV Video",
           framerate:25,
           showTime: true
       });
-
-      // start streaming
-      videoDataSource.connect();
   }
 }
 </script>
