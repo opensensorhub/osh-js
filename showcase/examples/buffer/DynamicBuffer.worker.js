dc9b366b43c5ee3d9bce70e46e4b8e04319da2b3
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2020-07-18 18:43:29 +0200

    Remove ability to read from broadcast channel into the DynamicBuffer. This will be part of a next PR

--- showcase/examples/buffer/DynamicBuffer.worker.js
+++ showcase/examples/buffer/DynamicBuffer.worker.js
@@ -1,18 +1,10 @@
 import DataSynchronizer from "../../../source/osh/buffer/DataSynchronizer";
 
-const bc = new BroadcastChannel('test_channel');
-
-const bcChannels = {};
-
 self.onmessage = (event) => {
     const buffer = new DataSynchronizer({
         replayFactor: 1,
         dataSources: event.data.dataSources
     });
-
-    bc.onmessage = function (event) {
-        self.buffer.push(event.data.id, event.data.data);
-    }
 
     buffer.onWait = (dataSourceId, time ,total) => {
         self.postMessage({
@@ -24,16 +16,11 @@ self.onmessage = (event) => {
     }
 
     buffer.onData = function (dataSourceId, data) {
-        if(!(dataSourceId in bcChannels)) {
-            bcChannels[dataSourceId] = new BroadcastChannel('test_channel-'+dataSourceId);
-        }
-        // const dataArray = str2ab(JSON.stringify(data));
         const objData = {
             message: 'data',
             dataSourceId: dataSourceId,
             data: data
         };
         self.postMessage(objData);
-        // bcChannels[dataSourceId].postMessage(objData);
     }
 }
