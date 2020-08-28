e49448df7028abfb57640e8138cb101bff4eb81e
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2020-08-28 15:15:05 +0200

    Fix not valid this for a worker, must use self instead causing getting an exception

--- source/osh/datareceiver/workers/VideoWithRoll.worker.js
+++ source/osh/datareceiver/workers/VideoWithRoll.worker.js
@@ -4,7 +4,7 @@ import VideoWithRollParser from "../parsers/VideoWithRoll.parser";
 const dataSourceHandler = new DataSourceHandler(new VideoWithRollParser());
 
 self.onmessage = (event) => {
-    dataSourceHandler.handleMessage(event.data, this);
+    dataSourceHandler.handleMessage(event.data, self);
 }
 
 
