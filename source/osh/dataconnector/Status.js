23367eca6cacc825424400694b590427e2bdd18e
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2020-09-26 23:41:49 +0200

    Add status enum object

---
+++ source/osh/dataconnector/Status.js
@@ -0,0 +1,9 @@
+/**
+ * Enum for connection status.
+ * @readonly
+ * @enum {{name: string}}
+ */
+export const Status = Object.freeze({
+    CONNECTED:   { name: "connected"},
+    DISCONNECTED:  { name: "disconnected"}
+});
