35a84e180a9eb000e7416ededb1f6dd3e25cef41
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2020-10-23 16:44:43 +0200

    Update sidebar to add advanced part

--- documentationV2/sidebars.js
+++ documentationV2/sidebars.js
@@ -1,26 +1,29 @@
 module.exports = {
-  someSidebar: {
-    'OpenSensorHub Toolkit': [
-        'intro',
-        'server',
-        'projects'
-    ],
-    'Getting Started': [
-        'getting_started/installation',
-        'getting_started/integration',
-        'getting_started/usage'
-    ],
-    'DataSources': [
-        'datasources/index',
-        'datasources/swejson',
-        'datasources/video',
-        'datasources/videoroll'
-    ],
-    'DataSynchronizer': [
-        'datasynchronizer/index'
-    ],
-    'View' : [
-        'view/index'
-    ]
-  }
+    someSidebar: {
+        'OpenSensorHub Toolkit': [
+            'intro',
+            'server',
+            'projects'
+        ],
+        'Getting Started': [
+            'getting_started/installation',
+            'getting_started/integration',
+            'getting_started/usage'
+        ],
+        'DataSources': [
+            'datasources/index',
+            'datasources/swejson',
+            'datasources/video',
+            'datasources/videoroll'
+        ],
+        'DataSynchronizer': [
+            'datasynchronizer/index'
+        ],
+        'View': [
+            'view/index'
+        ],
+        'Advanced': [
+            'advanced/datasynchronizer'
+        ]
+    }
 };
