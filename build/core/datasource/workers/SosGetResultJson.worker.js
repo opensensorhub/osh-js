b134264bc0e94921e3bb39831df82bde3cfeea1a
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2021-03-12 11:06:06 +0100

    remove local dev dir

--- build/core/datasource/workers/SosGetResultJson.worker.js
+++
@@ -1,7 +0,0 @@
-import SosGetResultJsonParser from '../parsers/SosGetResultJson.parser';
-import TimeSeriesDataSourceHandler from "./TimeSeriesDataSourceHandler";
-var dataSourceHandler = new TimeSeriesDataSourceHandler(new SosGetResultJsonParser());
-self.onmessage = function (event) {
-    dataSourceHandler.handleMessage(event.data, self);
-};
-//# sourceMappingURL=SosGetResultJson.worker.js.map
--- build/core/datasource/workers/SosGetResultVideoWithRoll.worker.js
+++
@@ -1,7 +0,0 @@
-import SosGetResultVideoWithRollParser from "../parsers/SosGetResultVideoWithRoll.parser";
-import TimeSeriesDataSourceHandler from "./TimeSeriesDataSourceHandler";
-var dataSourceHandler = new TimeSeriesDataSourceHandler(new SosGetResultVideoWithRollParser());
-self.onmessage = function (event) {
-    dataSourceHandler.handleMessage(event.data, self);
-};
-//# sourceMappingURL=SosGetResultVideoWithRoll.worker.js.map
