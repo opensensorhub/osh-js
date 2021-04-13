4105edaa3d085d8b26ca988268ae89dbcf71c8ec
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2021-04-13 21:27:07 +0200

    Update showcase Audio example

--- dev/showcase/js/audio.js
+++ dev/showcase/js/audio.js
@@ -1,59 +1,83 @@
 import SosGetResultAudio from "osh-js/core/datasource/SosGetResultAudio";
-import AudioTimeDomainCanvasView from "osh-js/core/ui/view/audio/AudioTimeDomainCanvasView";
-import AudioTimeDomainChartJSView from "osh-js/core/ui/view/audio/AudioTimeDomainChartJSView";
-import AudioFrequencyDomainCanvasView from "osh-js/core/ui/view/audio/AudioFrequencyDomainCanvasView";
 import AudioView from "osh-js/core/ui/view/audio/AudioView";
 
-function createView(replaySpeed) {
-  let audioDataSource = new SosGetResultAudio("alex-audio", {
-    protocol: "ws",
-    service: "SOS",
-    endpointUrl: "sensiasoft.net:8181/sensorhub/sos",
-    offeringID: "urn:android:device:dd90fceba7fd5b47-sos",
-    observedProperty: "http://sensorml.com/ont/swe/property/AudioFrame",
-    startTime: "2021-04-12T10:48:45Z",
-    endTime: "2021-04-12T10:49:45Z",
-    // startTime: "2021-03-24T09:56:10Z",
-    // endTime: "2021-03-24T09:56:30Z",
-    replaySpeed: replaySpeed
-  });
 
-  let audioView = new AudioView({
-    name: "Audio",
-    css: 'audio-css',
-    dataSourceId: audioDataSource.id
-  });
+let audioDataSource = new SosGetResultAudio("alex-audio", {
+  protocol: "ws",
+  service: "SOS",
+  endpointUrl: "sensiasoft.net:8181/sensorhub/sos",
+  offeringID: "urn:android:device:dd90fceba7fd5b47-sos",
+  observedProperty: "http://sensorml.com/ont/swe/property/AudioFrame",
+  startTime: "2021-04-12T10:48:45Z",
+  endTime: "2021-04-12T10:49:45Z",
+  // startTime: "2021-03-24T09:56:10Z",
+  // endTime: "2021-03-24T09:56:30Z",
+  replaySpeed: 1.0,
+  bufferingTime: 1000
+});
 
-  let audioTimeView = new AudioTimeDomainCanvasView({
-    container: "audio-canvas-time-container",
-    name: "Audio",
-    css: 'audio-css',
-    dataSourceId: audioDataSource.id
-  });
+let audioView = new AudioView({
+  name: "Audio",
+  css: 'audio-css',
+  dataSourceId: audioDataSource.id,
+  gain:10,
+  timeDomainVisualization: {
+    type: 'chart',
+    fftSize: 1024,
+    props: {
+      chartjsProps: {
+        chartProps: {
+          scales: {
+            yAxes: [{
+              scaleLabel: {
+                labelString: "Amplitude"
+              },
+              ticks: {
+                maxTicksLimit: 5
+              }
+            }],
+            xAxes: [{
+              scaleLabel: {
+                labelString: "Time"
+              },
+              ticks: {
+                maxTicksLimit: 130,
+                beginAtZero: true
+              }
+            }],
+          },
+          maintainAspectRatio: false
+        },
+        datasetsProps: {
+          pointRadius: 0.1,
+          borderColor: 'rgba(0,0,0,0.5)',
+          backgroundColor: 'rgba(255,195,100,0.2)',
+          barThickness: 5,
+          borderWidth: 1
+        },
+        datasetsMinMaxProps: {
+          pointRadius: 0.0,
+          backgroundColor: 'rgba(0,139,141,1.0)',
+          barThickness: 5,
+          borderWidth: 1
+        }
+      }
+    }
+  },
+  frequencyDomainVisualization: {
+    type: 'canvas',
+    fftSize: 32,
+    props: {
+      barWidth:20,
+      css: 'audio-css-canvas'
+    }
+  }
+});
 
-  let audioFrequencyView = new AudioFrequencyDomainCanvasView({
-    container: "audio-canvas-frequency-container",
-    name: "Audio",
-    css: 'audio-css',
-    dataSourceId: audioDataSource.id
-  });
-
-  let audioChartView = new AudioTimeDomainChartJSView({
-    container: "audio-chart-time-container",
-    name: "Audio",
-    css: 'audio-chart-css',
-    dataSourceId: audioDataSource.id
-  });
-
-  // create data source for UAV camera
+document.getElementById("listen").onclick = () => {
   audioDataSource.connect();
 }
 
-let first = true;
-document.getElementById("listen").onclick = () => {
-  createView(1.0)
-}
-
 document.getElementById("listen11").onclick = () => {
-  createView(1.1)
+  audioDataSource.connect();
 }
--- dev/site/showcase/js/audio.js
+++ dev/site/showcase/js/audio.js
@@ -1,5 +1,6 @@
-import AudioView from 'osh-js/core/ui/view/audio/AudioView.js';
 import SosGetResultAudio from "osh-js/core/datasource/SosGetResultAudio";
+import AudioView from "osh-js/core/ui/view/audio/AudioView";
+
 
 let audioDataSource = new SosGetResultAudio("alex-audio", {
   protocol: "ws",
@@ -7,29 +8,76 @@ let audioDataSource = new SosGetResultAudio("alex-audio", {
   endpointUrl: "sensiasoft.net:8181/sensorhub/sos",
   offeringID: "urn:android:device:dd90fceba7fd5b47-sos",
   observedProperty: "http://sensorml.com/ont/swe/property/AudioFrame",
-  startTime: "2021-03-24T09:56:00Z",
-  endTime: "now",
-  // replaySpeed: 1.5,
-  batchSize:500
+  startTime: "2021-04-12T10:48:45Z",
+  endTime: "2021-04-12T10:49:45Z",
+  // startTime: "2021-03-24T09:56:10Z",
+  // endTime: "2021-03-24T09:56:30Z",
+  replaySpeed: 1.0,
+  bufferingTime: 1000
 });
 
-// show it in video view
 let audioView = new AudioView({
-  container: "audio-container",
   name: "Audio",
-  dataSourceId: audioDataSource.id
+  css: 'audio-css',
+  dataSourceId: audioDataSource.id,
+  gain:10,
+  timeDomainVisualization: {
+    type: 'chart',
+    fftSize: 1024,
+    props: {
+      chartjsProps: {
+        chartProps: {
+          scales: {
+            yAxes: [{
+              scaleLabel: {
+                labelString: "Amplitude"
+              },
+              ticks: {
+                maxTicksLimit: 5
+              }
+            }],
+            xAxes: [{
+              scaleLabel: {
+                labelString: "Time"
+              },
+              ticks: {
+                maxTicksLimit: 130,
+                beginAtZero: true
+              }
+            }],
+          },
+          maintainAspectRatio: false
+        },
+        datasetsProps: {
+          pointRadius: 0.1,
+          borderColor: 'rgba(0,0,0,0.5)',
+          backgroundColor: 'rgba(255,195,100,0.2)',
+          barThickness: 5,
+          borderWidth: 1
+        },
+        datasetsMinMaxProps: {
+          pointRadius: 0.0,
+          backgroundColor: 'rgba(0,139,141,1.0)',
+          barThickness: 5,
+          borderWidth: 1
+        }
+      }
+    }
+  },
+  frequencyDomainVisualization: {
+    type: 'canvas',
+    fftSize: 32,
+    props: {
+      barWidth:20,
+      css: 'audio-css-canvas'
+    }
+  }
 });
 
 document.getElementById("listen").onclick = () => {
-  if(audioDataSource !== null) {
-    audioDataSource.disconnect();
-  }
-  // create data source for UAV camera
+  audioDataSource.connect();
+}
 
-  if(audioView !== null) {
-    // reset current view
-    audioView.reset();
-  }
-
+document.getElementById("listen11").onclick = () => {
   audioDataSource.connect();
 }
