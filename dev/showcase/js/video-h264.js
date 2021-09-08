05c5866042dc3e0a9f35dc4dd2f90d0fb547d8ab
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2021-09-08 20:29:54 +0200

    Update webcodec

--- dev/showcase/js/video-h264.js
+++ dev/showcase/js/video-h264.js
@@ -2,13 +2,13 @@ import SosGetResultVideo from 'osh-js/core/datasource/SosGetResultVideo.js';
 import FFMPEGView from 'osh-js/core/ui/view/video/FFMPEGView.js';
 // create data source for UAV camera
 let videoDataSource = new SosGetResultVideo("drone-Video", {
-  protocol: "ws",
-  service: "SOS",
-  endpointUrl: "sensiasoft.net:8181/sensorhub/sos",
-  offeringID: "urn:mysos:solo:video2",
-  observedProperty: "http://sensorml.com/ont/swe/property/VideoFrame",
-  startTime: "2015-12-19T21:04:30Z",
-  endTime: "2015-12-19T21:09:19Z",
+  protocol: 'ws',
+  service: 'SOS',
+  endpointUrl: 'sensiasoft.net:8181/sensorhub/sos',
+  offeringID: 'urn:mysos:solo:video2',
+  observedProperty: 'http://sensorml.com/ont/swe/property/VideoFrame',
+  startTime: '2015-12-19T21:04:29.231Z',
+  endTime: '2015-12-19T21:09:19.675Z',
   replaySpeed: 1
 });
 
