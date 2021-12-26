bfe95763c2a5ea8aa1e2a45bd829687e95e9be95
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2021-12-26 19:35:22 +0100

    Change Collection implementation to not store already fetched data.

--- source/core/sensorwebapi/api/Collection.js
+++ source/core/sensorwebapi/api/Collection.js
@@ -26,22 +26,23 @@ class Collection {
         this.pageSize = pageSize;
         this.parser = parser;
         this.connector = connector;
-        this.nextOffset = 0;
-        this.parseData = [];
+        this.currentOffset = 0;
+        this.nextOffset = undefined;
+        this.previousOffset = undefined;
         this.init = false;
-        this.cursorPosition = 0;
         this.total = 0;
+        this.data = [];
     }
 
     // cannot fetch dynamically data here otherwise we must change this function into async
     hasNext() {
-        return this.cursorPosition <= this.nextOffset || this.cursorPosition < this.parseData.length;
+        return !isDefined(this.nextOffset) || (this.nextOffset !== -1);
     }
 
-    async fetchData() {
+    async fetchData(offset) {
         const response = await this.connector.doRequest(
             this.url,
-            `${this.filter.toQueryString()}&offset=${this.nextOffset}&limit=${this.pageSize}`,
+            `${this.filter.toQueryString()}&offset=${offset}&limit=${this.pageSize}`,
             this.filter.props.responseType
             );
         await this.onFetchData(response);
@@ -54,55 +55,67 @@ class Collection {
     async parseResponse(encodedResponse) {
         let rec = JSON.parse(String.fromCharCode.apply(null, new Uint8Array(encodedResponse)));
 
+        this.data = [];
         for (let i = 0; i < rec.items.length; i++) {
-            this.parseData.push(await this.parser.parseData(rec.items[i]));
+            this.data.push(await this.parser.parseData(rec.items[i]));
         }
-        this.nextOffset = this.parseNextOffset(rec);
-
+        this.parseBoundsOffset(rec);
     }
 
-    parseNextOffset(response) {
+    parseBoundsOffset(response) {
         const links = response.links;
+        let next = false, previous = false;
         if (isDefined(links)) {
             // check all rel and find out 'next' property
             for (let i = 0; i < links.length; i++) {
                 if (links[i].rel === 'next') {
                     // update nextOffset
                     const url = new URL(links[i].href);
-                    return parseInt(url.searchParams.get('offset'));
+                    this.nextOffset = parseInt(url.searchParams.get('offset'));
+                    next = true;
+                }
+                if (links[i].rel === 'prev') {
+                    // update nextOffset
+                    const url = new URL(links[i].href);
+                    this.previousOffset = parseInt(url.searchParams.get('offset'));
+                    previous = true;
                 }
             }
         }
-        return -1;// max values
+        if(!next) {
+            this.nextOffset = -1;
+        }
+        if(!previous) {
+            this.previousOffset = -1;
+        }
     }
 
-    async nextPage(pageSize = undefined) {
-        // > 0 => 0 = start, -1 = maxValues
-        if (this.parseData.length === 0 || (this.nextOffset > 0 &&
-            (this.nextOffset > this.parseData.length
-            || this.cursorPosition === this.nextOffset))
-        ) {
-            await this.fetchData();
+    async nextPage(page = undefined) {
+        if(isDefined(page)) {
+            this.nextOffset = page * this.pageSize;
+        } else {
+            if(!isDefined(this.nextOffset)) {
+                this.nextOffset = 0;
+            }
         }
-        const length = Math.min(this.cursorPosition+this.pageSize, this.parseData.length);
-        const result = this.parseData.slice(this.cursorPosition, length);
-        this.cursorPosition += result.length;
-
-        return result;
+        await this.fetchData(this.nextOffset);
+        return this.data;
     }
 
-    previousPage(pageSize = undefined) {
-        this.cursorPosition = this.cursorPosition - this.pageSize;
-        let length = this.pageSize;
-        if(this.cursorPosition < 0 ){
-            length = this.cursorPosition + this.pageSize;
-            this.cursorPosition = 0;
+    async previousPage(page = undefined) {
+        if(isDefined(page)) {
+            this.previousOffset = page * this.pageSize;
+        } else {
+            if(!isDefined(this.previousOffset)) {
+                this.previousOffset = 0;
+            }
         }
-        return this.parseData.slice(this.cursorPosition, this.cursorPosition + length);
+        await this.fetchData(this.previousOffset);
+        return this.data;
     }
 
     hasPrevious() {
-        return this.cursorPosition > 0;
+        return !isDefined(this.previousOffset) || (this.previousOffset !== -1);
     }
 
 }
