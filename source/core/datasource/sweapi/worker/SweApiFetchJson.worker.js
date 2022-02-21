0b45906edd6fa83ddb4b477477eeabea6bfb4536
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2022-02-21 21:45:20 +0100

    Fix showcase examples given the Swe JSON parser modification

--- source/core/datasource/sweapi/worker/SweApiFetchJson.worker.js
+++ source/core/datasource/sweapi/worker/SweApiFetchJson.worker.js
@@ -1,7 +1,8 @@
 import SensorWebApiFetchHandler from "../handler/SweApiFetchHandler";
 import SweApiFetchStreamJsonParser from "../parser/json/SweApiFetchJson.parser";
+import SweApiFetchGenericJson from "../parser/json/SweApiFetchGenericJson.parser";
 
-let  dataSourceHandler = new SensorWebApiFetchHandler(new SweApiFetchStreamJsonParser());
+let  dataSourceHandler = new SensorWebApiFetchHandler(new SweApiFetchGenericJson());
 
 self.onmessage = (event) => {
     dataSourceHandler.handleMessage(event.data, self);
