27adca6126313cd84b1d8bd5d8e200d6afff2f9b
Christopher S. Dillard <csdillard@gmail.com>
2022-08-08 09:21:21 -0500

    FrustumLayer update to use base class utility method

--- source/core/ui/layer/FrustumLayer.js
+++ source/core/ui/layer/FrustumLayer.js
@@ -2,10 +2,10 @@ import Layer from "../../../../source/core/ui/layer/Layer";
 import {isDefined, randomUUID} from "../../../../source/core/utils/Utils";
 
 /**
- * Enumeration of orientation modes that indicate how the frustum's location and
- * orientation are provided by the layer.
+ * Enumeration of the ways that the frustum's position and orientation can be
+ * specified.
  */
-const OrientationMode = Object.freeze({
+const FrustumPositionMode = Object.freeze({
     /**
      * Constant indicating that the position provided to the frustum layer is
      * in the form of a latitude (in degrees), longitude (in degrees), and
@@ -37,76 +37,25 @@ class FrustumLayer extends Layer {
     constructor(properties) {
         super(properties);
         this.type = 'frustum';
-
-        this.properties = properties;
         this.props.frustumId = randomUUID();
-        this.props.color = 'rgb(255,0,0)';
-        this.opacity = 0.5;
-        this.origin = null;
-        this.fov = null;
-        this.near = 0.009;
-        this.range = null;
-        this.platformOrientation = {heading: 0.0, pitch: 0.0, roll: 0.0};
-        this.sensorOrientation = {yaw: 0.0, pitch: 0.0, roll: 0.0};
-        // Make sure there's a valid orientation mode set on this.properties
-        // by defaulting to LONLATALT_EULER_ANGLES.
-        this.properties.orientationMode = this.properties.orientationMode || OrientationMode.LONLATALT_EULER_ANGLES;
 
-        const that = this;
+        // Default position mode is LONLATALT_EULER_ANGLES, if not specified.
+        // Old code should continue to work as-is.
+        this.props.positionMode = this.properties.positionMode || FrustumPositionMode.LONLATALT_EULER_ANGLES;
 
-        if(isDefined(properties.color)){
-            this.props.color = properties.color;
-        }
-
-        if(isDefined(properties.opacity)){
-            this.props.opacity = properties.opacity;
-        }
-
-        if(isDefined(properties.getColor)) {
-            let fn = async (rec) => {
-                that.props.color = await  that.getFunc('getColor')(rec);
-            };
-            this.addFn(that.getDataSourcesIdsByProperty('getColor'),fn);
-        }
-
-        if(isDefined(properties.getOrigin)) {
-            let fn = async (rec) => {
-                that.props.origin = await that.getFunc('getOrigin')(rec);
-            };
-            this.addFn(that.getDataSourcesIdsByProperty('getOrigin'),fn);
-        }
-
-        if(isDefined(properties.getFov)) {
-            let fn = async (rec) => {
-                that.props.fov = await that.getFunc('getFov')(rec);
-            };
-            this.addFn(that.getDataSourcesIdsByProperty('getFov'),fn);
-        }
-
-        if(isDefined(properties.getRange)) {
-            let fn = async (rec) => {
-                that.props.range = await that.getFunc('getRange')(rec);
-            };
-            this.addFn(that.getDataSourcesIdsByProperty('getRange'),fn);
-        }
-
-        if(isDefined(properties.getPlatformOrientation)) {
-            let fn = async (rec) => {
-                that.props.platformOrientation = await that.getFunc('getPlatformOrientation')(rec);
-            };
-            this.addFn(that.getDataSourcesIdsByProperty('getPlatformOrientation'),fn);
-        }
-
-        if(isDefined(properties.getSensorOrientation)) {
-            let fn = async (rec) => {
-                that.props.sensorOrientation = await that.getFunc('getSensorOrientation')(rec);
-            };
-            this.addFn(that.getDataSourcesIdsByProperty('getSensorOrientation'),fn);
-        }
+        this.initDynamicProp("color", "rgb(255,0,0)");
+        this.initDynamicProp("opacity", 0.5);
+        this.initDynamicProp("origin");
+        this.initDynamicProp("fov");
+        this.initDynamicProp("aspectRatio", 4/3);
+        this.initDynamicProp("range");
+        this.initDynamicProp("rangeNear", 1);
+        this.initDynamicProp("platformOrientation");
+        this.initDynamicProp("sensorOrientation");
 
         this.saveState();
     }
 }
 
 export default FrustumLayer;
-export { OrientationMode };
+export { FrustumPositionMode };
