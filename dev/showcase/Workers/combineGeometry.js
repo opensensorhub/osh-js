d5b7d65205dabdb650c11c358f3fc7a1358a3aeb
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2022-10-03 12:25:26 +0200

    Update for #664

--- dev/showcase/Workers/combineGeometry.js
+++ dev/showcase/Workers/combineGeometry.js
@@ -1,13 +1,1 @@
-/* This file is automatically rebuilt by the Cesium build process. */
-define(['./when-e6e3e713', './Check-1df6b9a0', './Math-c5f6c994', './Cartesian2-1d7364fa', './Transforms-943e8463', './RuntimeError-717c34db', './WebGLConstants-7f7d68ac', './ComponentDatatype-2b8834a4', './GeometryAttribute-3a303898', './GeometryAttributes-6cf4559b', './AttributeCompression-d68d64ef', './GeometryPipeline-2d6c52b2', './EncodedCartesian3-d723731d', './IndexDatatype-e2961542', './IntersectionTests-c05f88ce', './Plane-2e419ea5', './PrimitivePipeline-efe6e32e', './WebMercatorProjection-2eb538cc', './createTaskProcessorWorker'], function (when, Check, _Math, Cartesian2, Transforms, RuntimeError, WebGLConstants, ComponentDatatype, GeometryAttribute, GeometryAttributes, AttributeCompression, GeometryPipeline, EncodedCartesian3, IndexDatatype, IntersectionTests, Plane, PrimitivePipeline, WebMercatorProjection, createTaskProcessorWorker) { 'use strict';
-
-    function combineGeometry(packedParameters, transferableObjects) {
-            var parameters = PrimitivePipeline.PrimitivePipeline.unpackCombineGeometryParameters(packedParameters);
-            var results = PrimitivePipeline.PrimitivePipeline.combineGeometry(parameters);
-            return PrimitivePipeline.PrimitivePipeline.packCombineGeometryResults(results, transferableObjects);
-        }
-    var combineGeometry$1 = createTaskProcessorWorker(combineGeometry);
-
-    return combineGeometry$1;
-
-});
+define(["./when-c2e8ef35","./Check-c4f3a3fc","./Math-d30358ed","./Cartesian2-e875d9d2","./Transforms-44592b02","./RuntimeError-6122571f","./WebGLConstants-4ae0db90","./ComponentDatatype-5d3f6452","./GeometryAttribute-4098b8de","./GeometryAttributes-57608efc","./AttributeCompression-6cb5b251","./GeometryPipeline-99c06fbd","./EncodedCartesian3-e19aab62","./IndexDatatype-e3260434","./IntersectionTests-ef7d18d8","./Plane-2d882f9f","./PrimitivePipeline-a7956312","./WebMercatorProjection-44bf888f","./createTaskProcessorWorker"],function(e,t,i,r,n,o,a,s,c,m,b,f,d,P,p,u,y,C,l){"use strict";return l(function(e,t){var i=y.PrimitivePipeline.unpackCombineGeometryParameters(e),r=y.PrimitivePipeline.combineGeometry(i);return y.PrimitivePipeline.packCombineGeometryResults(r,t)})});
