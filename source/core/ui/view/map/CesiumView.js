966d25843ab735b49d3f0d81e2a6c939b0ea15d0
mdh <mathieu.dhainaut@gmail.com>
2022-12-07 17:32:08 +0100

    Add support for dynamic changing of icon Size & color into the Cesium… (#702)
    * Add support for dynamic changing of icon Size & color into the CesiumView
    * Add support for iconScale into the CesiumView

--- source/core/ui/view/map/CesiumView.js
+++ source/core/ui/view/map/CesiumView.js
@@ -444,11 +444,11 @@ class CesiumView extends MapView {
                 eyeOffset: new Cartesian3(0, 0, -1 * properties.zIndex), // make sure icon always displays in front,
                 show: properties.visible,
                 heightReference: properties.defaultToTerrainElevation ? HeightReference.CLAMP_TO_GROUND : HeightReference.NONE,
-                scale: 1.0,
+                scale: properties.iconScale,
                 imageSubRegion: undefined,
-                color: undefined,
-                width: undefined,
-                height: undefined,
+                color: color,
+                width: properties.iconSize[0],
+                height: properties.iconSize[1],
                 translucencyByDistance: undefined,
                 sizeInMeters: undefined,
                 distanceDisplayCondition: undefined,
@@ -551,6 +551,7 @@ class CesiumView extends MapView {
             entity.description = entityOpts.description;
             entity.orientation = entityOpts.orientation;
 
+            console.log(entity.billboard)
             return entity;
         }
     }
