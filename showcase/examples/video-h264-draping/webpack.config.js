c2ece4b40d326a4e9deab1555b5843fad336946a
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2024-03-06 19:16:05 +0100

    Update cesium example to 1.115.0

--- showcase/examples/video-h264-draping/webpack.config.js
+++ showcase/examples/video-h264-draping/webpack.config.js
@@ -1,106 +1,103 @@
-/* webpack.config.js */
-
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+// webpack.config.common.js
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const CopywebpackPlugin = require('copy-webpack-plugin');
+const { DefinePlugin, ProvidePlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+// Common configs
+const path = require('path');
+const webpack = require('webpack');
+const nodePolyfillWebpackPlugin = require('node-polyfill-webpack-plugin')
 // Cesium deps
 const cesiumSource = 'node_modules/cesium/Source';
 const cesiumWorkers = '../Build/Cesium/Workers';
-//
 
-var path = require('path');
 const PROCESS_BASE_PATH = process.cwd();
 
-module.exports = {
-    // Tell Webpack which file kicks off our app.
-    entry: path.resolve(__dirname,'video-h264-draping.js'),
-    // Tell Weback to output our bundle to ./dist/bundle.js
+// Now, using the cesiumConfig in your real configuration
+const config = {
+    entry: {
+        entry: path.resolve(__dirname,'video-h264-draping.js'),
+    },
     output: {
-        filename: 'bundle.video.h264.draping.js',
+        filename: 'bundle.[name].video.h264.draping.js',
         path: path.resolve(__dirname, 'dist'),
-        // Needed to compile multiline strings in Cesium
         sourcePrefix: ''
     },
-    amd: {
-        // Enable webpack-friendly use of require in Cesium
-        toUrlUndefined: true
-    },
-    node: {
-        // Resolve node module use of fs
-        fs: 'empty'
-    },
-    // Tell Webpack which directories to look in to resolve import statements.
-    // Normally Webpack will look in node_modules by default but since we’re overriding
-    // the property we’ll need to tell it to look there in addition to the
-    // bower_components folder.
     resolve: {
         modules: [
             path.resolve(__dirname, 'node_modules'),
         ],
         alias: {
-            'osh-js': path.resolve(__dirname, '../../../source')
+            'osh-js': path.resolve(__dirname, '../../../source'),
+            'cesium': path.resolve(__dirname, 'node_modules/cesium'),
+        },
+        fallback : {
+            "path": require.resolve("path-browserify"),
+            "crypto": false,
+            fs: false
         }
     },
-    // These rules tell Webpack how to process different module types.
-    // Remember, *everything* is a module in Webpack. That includes
-    // CSS, and (thanks to our loader) HTML.
     module: {
+        unknownContextCritical : false,
+    },
+    node: {
+    },
+    amd: {
+        // Enable webpack-friendly use of require in Cesium
+        toUrlUndefined: true
+    },
+    devServer: {
+        https: true,
+        compress: false,
+        hot: true
+    },
+    module: {
+        unknownContextCritical: false,
         rules: [
             {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
-            },{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.worker\.js$/,
-                use: { loader: 'worker-loader', options: { name: 'Worker.[hash].js' } }
-            }
-        ]
+                use: { loader: 'worker-loader' }
+            },
+        ],
     },
-    // Enable the Webpack dev server which will build, serve, and reload our
-    // project on changes.
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000,
-        hot: true,
-        index: 'video-h264-draping.html',
-        https:true
-    },
-    devtool: 'source-map',
     plugins: [
-        /**
-         * All files inside webpack's output.path directory will be removed once, but the
-         * directory itself will not be. If using webpack 4+'s default configuration,
-         * everything under <PROJECT_DIR>/dist/ will be removed.
-         * Use cleanOnceBeforeBuildPatterns to override this behavior.
-         *
-         * During rebuilds, all webpack assets that are not used anymore
-         * will be removed automatically.
-         *
-         * See `Options and Defaults` for information
-         */
         new CleanWebpackPlugin(),
-        // This plugin will generate an index.html file for us that can be used
-        // by the Webpack dev server. We can give it a template file (written in EJS)
-        // and it will handle injecting our bundle for us.
         new HtmlWebpackPlugin({
-            filename: 'video-h264-draping.html',
-            template: path.resolve(__dirname, 'video-h264-draping.html')
+            template: path.resolve(__dirname, 'video-h264-draping.html'),
+            filename: 'video-h264-draping.html'
         }),
-        // This plugin will copy files over to ‘./dist’ without transforming them.
-        // That's important because the custom-elements-es5-adapter.js MUST
-        // remain in ES2015. We’ll talk about this a bit later :)
-        new CopyWebpackPlugin([
-            { from: path.resolve(__dirname,'models'), to: 'models'},
-            { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, cesiumWorkers), to: 'Workers', force:true },
-            { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'Assets'), to: 'Assets', force:true },
-            { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'Widgets'), to: 'Widgets', force:true }
-        ])
-    ]
+        new DefinePlugin({
+            // Define relative base path in cesium for loading assets
+            BASE_URL: JSON.stringify('/')
+        }),
+        new   CopywebpackPlugin({
+            patterns: [
+                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'ThirdParty'), to: 'ThirdParty', force:true },
+                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'Assets'), to: 'Assets', force:true },
+                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'Widgets'), to: 'Widgets', force:true }
+            ]
+        }),
+        new nodePolyfillWebpackPlugin(), // fix Webpack Buffer not defined
+    ],
 };
+
+module.exports = config;
