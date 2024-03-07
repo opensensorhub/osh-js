408f98ce96c70580ad943b05bf12978ac5150a42
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2024-03-07 11:19:43 +0100

    Make working version of cesium + wepack 5

--- showcase/examples/video-h264-draping/video-h264-draping.js
+++ showcase/examples/video-h264-draping/video-h264-draping.js
@@ -1,4 +1,4 @@
-import SosGetResult from 'osh-js/core/datasource/sos/SosGetResult.datasource.js';
+/*import SosGetResult from 'osh-js/core/datasource/sos/SosGetResult.datasource.js';
 import CesiumView from 'osh-js/core/ui/view/map/CesiumView.js';
 import {
     EllipsoidTerrainProvider,
@@ -166,15 +166,15 @@ let cesiumView = new CesiumView({
     },
 });
 
-cesiumView.viewer.terrainProvider = new EllipsoidTerrainProvider();
-cesiumView.viewer.scene.logarithmicDepthBuffer = false;
-cesiumView.viewer.camera.setView({
-    destination : Cartesian3.fromDegrees(-86.5812,34.6904,1000)
-});
+// cesiumView.viewer.terrainProvider = new EllipsoidTerrainProvider();
+// cesiumView.viewer.scene.logarithmicDepthBuffer = false;
+// cesiumView.viewer.camera.setView({
+//     destination : Cartesian3.fromDegrees(-86.5812,34.6904,1000)
+// });
 
 // select bing maps as default imagery
-const baseLayerPickerViewModel = cesiumView.viewer.baseLayerPicker.viewModel;
-baseLayerPickerViewModel.selectedImagery = baseLayerPickerViewModel.imageryProviderViewModels[0];
+// const baseLayerPickerViewModel = cesiumView.viewer.baseLayerPicker.viewModel;
+// baseLayerPickerViewModel.selectedImagery = baseLayerPickerViewModel.imageryProviderViewModels[0];
 
 // start streaming
 const dataSynchronizer = new DataSynchronizer({
@@ -189,5 +189,42 @@ const dataSynchronizer = new DataSynchronizer({
         gimbalOrientationDataSource
     ]
 });
-dataSynchronizer.connect()
+dataSynchronizer.connect()*/
+
+import {
+    Viewer,
+    Cartesian3,
+    Math,
+    Terrain,
+    createOsmBuildingsAsync,
+    CesiumWidget
+} from "@cesium/engine";
+import "@cesium/engine/Source/Widget/CesiumWidget.css";
+
+import "./main.css";
+
+// CesiumJS has a default access token built in but it's not meant for active use.
+// please set your own access token can be found at: https://cesium.com/ion/tokens.
+// Ion.defaultAccessToken = "YOUR TOKEN HERE";
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new CesiumWidget("cesium-h264-draping-container", {
+    terrain: Terrain.fromWorldTerrain(),
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+createOsmBuildingsAsync().then(osmBuildingsTileset => {
+    viewer.scene.primitives.add(osmBuildingsTileset);
+
+});
+
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+    destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+    orientation: {
+        heading: Math.toRadians(0.0),
+        pitch: Math.toRadians(-15.0),
+    },
+});
+
 
--- showcase/examples/video-h264-draping/webpack.config.js
+++ showcase/examples/video-h264-draping/webpack.config.js
@@ -1,26 +1,25 @@
 // webpack.config.common.js
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const CopywebpackPlugin = require('copy-webpack-plugin');
-const { DefinePlugin, ProvidePlugin } = require('webpack');
+const { DefinePlugin } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // Common configs
 const path = require('path');
-const webpack = require('webpack');
-const nodePolyfillWebpackPlugin = require('node-polyfill-webpack-plugin')
 // Cesium deps
-const cesiumSource = 'node_modules/cesium/Source';
-const cesiumWorkers = '../Build/Cesium/Workers';
+const cesiumSource = 'node_modules/cesium/Build/Cesium';
+const cesiumWorkers = 'node_modules/@cesium/engine/Source';
 
 const PROCESS_BASE_PATH = process.cwd();
+const cesiumBaseUrl = "cesiumStatic";
 
 // Now, using the cesiumConfig in your real configuration
 const config = {
+    context: __dirname,
     entry: {
         entry: path.resolve(__dirname,'video-h264-draping.js'),
     },
     output: {
-        filename: 'bundle.[name].video.h264.draping.js',
+        filename: 'bundle.video.h264.draping.js',
         path: path.resolve(__dirname, 'dist'),
         sourcePrefix: ''
     },
@@ -29,26 +28,17 @@ const config = {
             path.resolve(__dirname, 'node_modules'),
         ],
         alias: {
-            'osh-js': path.resolve(__dirname, '../../../source'),
-            'cesium': path.resolve(__dirname, 'node_modules/cesium'),
+            'osh-js': path.resolve(__dirname, '../../../source')
         },
         fallback : {
             "path": require.resolve("path-browserify"),
             "crypto": false,
             fs: false
-        }
-    },
-    module: {
-        unknownContextCritical : false,
-    },
-    node: {
-    },
-    amd: {
-        // Enable webpack-friendly use of require in Cesium
-        toUrlUndefined: true
+        },
+        mainFiles: ["index", "Cesium"],
     },
     devServer: {
-        https: true,
+        https: false,
         compress: false,
         hot: true
     },
@@ -56,26 +46,12 @@ const config = {
         unknownContextCritical: false,
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
             },
             {
-                test: /\.(png|woff|woff2|eot|ttf|svg|jpg|gif)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]?[hash]'
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.worker\.js$/,
-                use: { loader: 'worker-loader' }
+                test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+                type: "asset/inline",
             },
         ],
     },
@@ -87,17 +63,19 @@ const config = {
         }),
         new DefinePlugin({
             // Define relative base path in cesium for loading assets
-            BASE_URL: JSON.stringify('/')
+            CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
         }),
         new   CopywebpackPlugin({
             patterns: [
-                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'ThirdParty'), to: 'ThirdParty', force:true },
-                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'Assets'), to: 'Assets', force:true },
-                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'Widgets'), to: 'Widgets', force:true }
+                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'ThirdParty'), to: `${cesiumBaseUrl}/ThirdParty`, force:true },
+                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'Workers'), to: `${cesiumBaseUrl}/Workers`, force:true },
+                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'Assets'), to: `${cesiumBaseUrl}/Assets`, force:true },
+                { from: path.join(PROCESS_BASE_PATH+'/'+cesiumSource, 'Widgets'), to: `${cesiumBaseUrl}/Widgets`, force:true }
             ]
         }),
-        new nodePolyfillWebpackPlugin(), // fix Webpack Buffer not defined
     ],
+    mode: "development",
+    devtool: "eval",
 };
 
 module.exports = config;
